691ef809f2b52759ec17c7041465eb867a229f87
Phy <git@phy25.com>
2018-09-16 21:24:40 -0400

    Shell arguments made

--- TweetPI.py
+++ TweetPI.py
@@ -1,12 +1,16 @@
 #!/usr/bin/env python
-# TweetPI library by Phy25
+"""
+Module TweetPI, by @phy25
+"""
+
+import sys
 
 class TweetPI:
     def __init__(self, options):
         pass
 
     def get_timeline(self, username, page, limit):
-        return PhotoList(l=list(), source="timeline")
+        return PhotoList(list=list(), source="timeline")
 
 class Photo:
     local_folder = None
@@ -38,3 +42,78 @@ class PhotoList:
 
     def get_annotations(self):
         return list()
+
+    def get_list(self):
+        return self.l
+
+def shell_list(args):
+    tpi = TweetPI({})
+    try:
+        if 'timeline' in args:
+            photolist = tpi.get_timeline(username=args.timeline, page=1, limit=args.limit)
+        else:
+            sys.exit(1)
+    except Exception as e:
+        print(e, file=sys.stderr)
+        sys.exit(2)
+
+    for t in photolist.get_list():
+        print(t)
+
+def shell_download(args):
+    tpi = TweetPI({})
+    try:
+        if 'timeline' in args:
+            photolist = tpi.get_timeline(username=args.timeline, page=1, limit=args.limit)
+            result = photlist.download_all()
+        else:
+            sys.exit(1)
+    except Exception as e:
+        print(e, file=sys.stderr)
+        sys.exit(2)
+
+def shell_video(args):
+    pass
+
+def shell_annotate(args):
+    pass
+
+
+def main(argv=None):
+    import argparse
+    if not argv:
+        argv = sys.argv[1:]
+
+    argparser = argparse.ArgumentParser(prog="TweetPI.py", description='Tweet Photo Insight: Python API to get photos in Twitter feed, with a video and photo annotations.')
+    subparsers = argparser.add_subparsers(help=".")
+
+    parser_list = subparsers.add_parser('list', help='list images in Twitter feed')
+    parser_list.add_argument('--timeline', required=True, help="from someone's timeline")
+    parser_list.add_argument('--limit', help="tweets limit")
+    parser_list.set_defaults(func=shell_list)
+
+    parser_download = subparsers.add_parser('download', help='download images in Twitter feed')
+    parser_download.add_argument('--timeline', required=True, help="from someone's timeline")
+    parser_download.add_argument('--limit', help="tweets limit")
+    parser_download.set_defaults(func=shell_download)
+
+    parser_video = subparsers.add_parser('video', help='generate a video from images in Twitter feed')
+    parser_video.add_argument('--timeline', required=True, help="from someone's timeline")
+    parser_video.add_argument('--limit', help="tweets limit")
+    parser_video.set_defaults(func=shell_video)
+
+    parser_annotate = subparsers.add_parser('annotate', help='get annotations of images in Twitter feed')
+    parser_annotate.add_argument('--timeline', required=True, help="from someone's timeline")
+    parser_annotate.add_argument('--limit', help="tweets limit")
+    parser_annotate.set_defaults(func=shell_annotate)
+
+    if len(argv) == 0:
+        argparser.print_help(sys.stderr)
+        sys.exit(1)
+
+    args = argparser.parse_args(args=argv)
+    if 'func' in args:
+        args.func(args)
+
+if __name__ == "__main__":
+    main()
