9f2aa9fa4e0463568b8aea9cec8b31c0e05a028f
Phy <git@phy25.com>
2018-11-25 23:43:55 -0500

    Update docs for v1.0

--- README_demo.py
+++ README_demo.py
@@ -11,23 +11,27 @@ with open("options.json", "r") as fp:
 # o = {"twitter_consumer_key":"...", "twitter_consumer_secret":"...", "twitter_access_token":"...", "twitter_access_secret":"...", "google_key_json":"gapi.json"}
 tpi = TweetPI(o)
 
-try:
-    # list
-    photolist = tpi.get_timeline(username="football", page=1, limit=50)
-    print(photolist)
-    # download
-    photolist.download_all(shell=True)
-    # annotate
-    photolist.get_annotations()
-    for t in photolist.get_list():
-        print('{}: {}'.format(t.remote_url, ", ".join([a.description for a in t.annotation.label_annotations])))
-    # video
-    videopath = video.generate_video(photos=photolist, name='video1.mp4', size='1080x720', shell=True, interval=3)
-    print(videopath)
-    # annotated video
-    videopath2 = video.generate_annotated_video(photos=photolist, name='video2.mp4', size='1080x720', shell=True, font_color='rgb(255,0,0)', font_file='Roboto-Regular.ttf', interval=3, font_size=30)
-    print(videopath2)
-except Exception as e:
-    # Error handling
-    print(e, file=sys.stderr)
-    sys.exit(2)
+def main():
+    try:
+        # list
+        photolist = tpi.get_timeline(username="football", page=1, limit=50)
+        print(photolist)
+        # download
+        photolist.download_all(shell=True)
+        # annotate
+        photolist.get_annotations()
+        for t in photolist.photos:
+            print('{}: {}'.format(t.remote_url, ", ".join([a.description for a in t.annotation.label_annotations])))
+        # video
+        videopath = video.generate_video(photos=photolist, name='video1.mp4', size='1080x720', shell=True, interval=3)
+        print(videopath)
+        # annotated video
+        videopath2 = video.generate_annotated_video(photos=photolist, name='video2.mp4', size='1080x720', shell=True, font_color='rgb(255,0,0)', font_file='Roboto-Regular.ttf', interval=3, font_size=30)
+        print(videopath2)
+    except Exception as e:
+        # Error handling
+        print("ERROR: {}".format(e), file=sys.stderr)
+        sys.exit(2)
+
+if __name__ == "__main__":
+    main()
